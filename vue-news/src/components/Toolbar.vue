1676847411c70cae9e8017a059b2dd2d7825b1aa
asd0755 <asd0755@naver.com>
2021-01-08 10:47:14 +0900

    Added ToolBar

---
+++ vue-news/src/components/Toolbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <router-link to="news">News</router-link> | 
+    <router-link to="ask">Ask</router-link> |
+    <router-link to="jobs">Jobs</router-link>
+  </div>
+</template>
+
+<style scoped>
+.header {
+    color: white;
+    background-color: #42bB83;
+    display: flex;
+    padding: 8px;    
+}
+/* 개발자도구 href 클래스명 */
+.header .router-link-exact-active {
+    color: #35495e;
+}
+.header a {
+    color: white
+}
+</style>
