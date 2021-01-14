5ac69cc038330ee085ae1fc2590bd205ca96da7b
asd0755 <asd0755@naver.com>
2021-01-14 11:48:50 +0900

    Added API for axios fetch data

---
+++ vue-news/src/api/index.js
@@ -0,0 +1,26 @@
+import axios from '../../node_modules/axios';
+
+// 1 - HTTP Request & Response 관련 기본 설정
+const config = {
+    baseUrl: 'https://api.hnpwa.com/v0/'
+};
+
+// 2 - API 함수 정리
+function fetchNewsList() {
+    // return axios.get(config.baseUrl + 'news/1.json')
+    return axios.get(`${config.baseUrl}news/1.json`)
+};
+
+function fetchAskList() {
+    return axios.get(`${config.baseUrl}ask/1.json`)
+};
+
+function fetchJobsList() {
+    return axios.get(`${config.baseUrl}jobs/1.json`)
+};
+
+export {
+    fetchNewsList,
+    fetchAskList,
+    fetchJobsList
+}
