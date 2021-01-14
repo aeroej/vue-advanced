4b69ff8146a0807f78ca894b1ada2d4272016658
asd0755 <asd0755@naver.com>
2021-01-14 11:53:25 +0900

    Added vuex module to NewsView.vue

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-      news
+    <div v-for="user in this.$store.state.news">{{ user.title }}</div>
   </div>
 </template>
 
 <script>
-export default {
+import { fetchNewsList } from '../api/index.js';
 
+export default {
+  created() {
+    // store/index.js의 FETCH_NEWS 실행   fetchNewList()   axios와 console.log
+    this.$store.dispatch('FETCH_NEWS');
+
+    /* vuex쓰기 전
+    fetchNewsList()
+      .then(response => this.users = response.data)
+      .catch(error => console.log(error));
+    */
+  },
 }
 </script>
 
 <style>
-
 </style>
