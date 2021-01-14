72a289ecd5de05973c464374f2d20874983eca4c
asd0755 <asd0755@naver.com>
2021-01-14 11:50:53 +0900

    Installed vuex store

--- vue-news/src/main.js
+++ vue-news/src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { router } from './routes/index.js';
+import { store } from '../src/store/index.js'
 
 Vue.config.productionTip = false
-
 
 new Vue({
   render: h => h(App),
   router: router,
+  store: store,
 }).$mount('#app')
