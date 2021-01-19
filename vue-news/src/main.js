a2533b3fda8d94fd7b93ec128128de2c3f8d89e7
asd0755 <asd0755@naver.com>
2021-01-19 10:25:46 +0900

    init repository with advertising image

--- vue-news/src/main.js
+++ vue-news/src/main.js
@@ -1,13 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { router } from './routes/index.js';
-import { store } from '../src/store/index.js'
+import Vue from "vue";
+import App from "./App.vue";
+import { router } from "./routes/index.js";
+import { store } from "../src/store/index.js";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 // router & store 축약가능
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
   router: router,
   store: store,
-}).$mount('#app')
+}).$mount("#app");
